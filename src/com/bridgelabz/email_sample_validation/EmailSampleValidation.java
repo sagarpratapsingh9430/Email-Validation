01514b2fb6df8cb7b29396d442ed040cc45e66bf
Sagar Pratap Singh <sagarpratapsingh9432@gmail.com>
2023-07-11 09:34:54 +0530

    Email Smaple Validation

---
+++ src/com/bridgelabz/email_sample_validation/EmailSampleValidation.java
@@ -0,0 +1,29 @@
+package com.bridgelabz.email_sample_validation;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailSampleValidation {
+    void validEmailSample(){
+        Pattern pattern = Pattern.compile("[a-z]+([+-._]{0,1}[a-z0-9])*@[a-z0-9]+[.][a-z]{2,3}([.]{0,1}[a-z]{2,4})*");
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+        System.out.print("Enter email sample: ");
+        String email = sc.nextLine();
+        Matcher matcher = pattern.matcher(email);
+        if (matcher.matches()){
+            System.out.print("Email is valid: " + email);
+        }else {
+            System.out.print("Not valid: ");
+        }
+    }
+    public static void main(String[] args) {
+        EmailSampleValidation obj = new EmailSampleValidation();
+
+        obj.validEmailSample();
+
+    }
+
+
+}
